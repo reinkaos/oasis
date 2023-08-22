204a0853fbb62ec714c766275cfa93ff0dad67b3
reinkaos <rahulbmn.0118@gmail.com>
2023-08-22 23:04:12 +0530

    first

---
+++ personal-finance-tracker/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('Login clicked');
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="title">Personal Finance Tracker</h1>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+        <p>Don't have an account? <a href="#">Register here</a></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
