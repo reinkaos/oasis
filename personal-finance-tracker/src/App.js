822df5ba0431df66e329c654d0f9be719a49634a
reinkaos <rahulbmn.0118@gmail.com>
2023-08-22 23:10:23 +0530

    updates

--- personal-finance-tracker/src/App.js
+++ personal-finance-tracker/src/App.js
@@ -1,39 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('Login clicked');
-  };
-
   return (
     <div className="App">
       <div className="container">
         <h1 className="title">Personal Finance Tracker</h1>
-        <form onSubmit={handleLogin}>
+        <form>
           <div className="form-group">
             <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+            <input type="email" id="email" required />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <input type="password" id="password" required />
           </div>
           <button type="submit">Log In</button>
         </form>
